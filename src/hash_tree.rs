95ba9bbdd5353f2dd8b0f54225229fcef7806046
Lee Olayvar <leeola@fastmail.com>
2020-07-21 14:59:53 -0700

    toying with tree pushing

--- src/hash_tree.rs
+++ src/hash_tree.rs
@@ -1,19 +1,84 @@
 use {
-    crate::{Fixity, Result},
+    crate::{Addr, ContentAddrs, ContentNode, Fixity, Result},
     fastcdc::Chunk,
+    std::mem,
 };
+#[derive(Debug)]
+pub struct HashTree {
+    depth: usize,
+    root_node: Node,
+}
+impl HashTree {
+    pub fn new(max_hashes: usize) -> Self {
+        Self {
+            depth: 0,
+            root_node: Node::new(max_hashes, None),
+        }
+    }
+    pub fn push<A: Into<Addr>>(hash: A) -> Option<ContentNode> {
+        // let hash = Addr::from(hash);
+        // self.root_node.push(hash);
+        todo!()
+    }
+}
 #[derive(Debug)]
 pub struct Node {
     depth: usize,
-    max_children: usize,
-    size: usize,
+    max_hashes: usize,
+    hashes: Vec<Addr>,
+    child: Option<Box<Node>>,
+    state: NodeState,
 }
 impl Node {
-    pub fn new<S>(
-        fixity: Fixity<S>,
-        data: &[u8], impl Iterator<Item
-iter: impl Iterator<Item = Chunk>,
-    ) -> Result<Self> {
+    pub fn new(max_hashes: usize, child: Option<Node>) -> Self {
+        Self {
+            depth: 0,
+            max_hashes,
+            child: child.map(Box::new),
+            hashes: Vec::new(),
+            state: NodeState::ReceiveHash,
+        }
+    }
+    pub fn into_parent(self) -> Self {
         todo!()
     }
+    pub fn push(&mut self, hash: Addr) -> Option<ContentNode> {
+        match (self.depth, self.state) {
+            (0, NodeState::ReceiveHash) => {
+                self.hashes.push(hash);
+                if self.hashes.len() == self.max_hashes {
+                    let hashes = mem::replace(&mut self.hashes, Vec::new());
+                    return Some(ContentNode {
+                        children: ContentAddrs::Chunks(hashes),
+                    });
+                }
+                return None;
+            }
+            (_, NodeState::ReceiveHash) => {
+                self.state = NodeState::ProxyHash;
+                self.hashes.push(hash);
+                if self.hashes.len() == self.max_hashes {
+                    let hashes = mem::replace(&mut self.hashes, Vec::new());
+                    return Some(ContentNode {
+                        children: ContentAddrs::Nodes(hashes),
+                    });
+                }
+                return None;
+            }
+            (0, NodeState::ProxyHash) => unreachable!(),
+            (_, NodeState::ProxyHash) => {
+                let child = self.child.as_mut().expect("proxy missing child");
+                let hashes = child.push(hash);
+                if hashes.is_some() {
+                    self.state = NodeState::ReceiveHash;
+                }
+            }
+        }
+        todo!()
+    }
+}
+#[derive(Debug)]
+enum NodeState {
+    ReceiveHash,
+    ProxyHash,
 }
