98ef653a8f9adb504b888fe8d75d98e35f4708f4
Lee Olayvar <leeola@fastmail.com>
2020-08-09 10:31:29 -0700

    add fixi web

--- fixi_cli/src/bin/fixi.rs
+++ fixi_cli/src/bin/fixi.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "web")]
+use fixi_web::Config as WebConfig;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -6,9 +8,14 @@ struct Opt {
     #[structopt(subcommand)]
     cmd: Command,
 }
-
 #[derive(Debug, StructOpt)]
 enum Command {
+    Raw(RawCommand),
+    #[cfg(feature = "web")]
+    Web(WebConfig),
+}
+#[derive(Debug, StructOpt)]
+enum RawCommand {
     Get {},
     Put {
         #[structopt(long)]
@@ -17,7 +24,12 @@ enum Command {
     Fetch {},
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> () {
     let opt = Opt::from_args();
-    println!("{:?}", opt);
+    match opt.cmd {
+        Command::Raw(_) => unimplemented!("raw cmd"),
+        #[cfg(feature = "web")]
+        Command::Web(c) => fixi_web::serve(c).await,
+    }
 }
