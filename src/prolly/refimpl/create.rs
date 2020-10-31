2cd872237467ce93485e69bf15a8812b41fbc0a0
Lee Olayvar <leeola@fastmail.com>
2020-10-31 10:09:45 -0700

    sync

--- src/prolly/refimpl/create.rs
+++ src/prolly/refimpl/create.rs
@@ -44,7 +44,6 @@ where
         // unstable should be fine, since the keys will (soon) be unique.
         kvs.sort_unstable_by(|a, b| a.0.cmp(&b.0));
         // Ensure the kvs are unique.
-        {
             let maybe_dups_len = kvs.len();
             kvs.dedup_by(|a, b| a.0 == b.0);
             if maybe_dups_len != kvs.len() {
@@ -52,121 +51,14 @@ where
                     message: "cannot construct prolly tree with non-unique keys".to_owned(),
                 });
             }
-        }
-        let mut block_addrs = Vec::new();
-        let mut block_buf = Vec::new();
-        for kv in kvs.into_iter() {
-            let boundary = self.roller.roll_bytes(&crate::value::serialize(&kv)?);
-            block_buf.push(kv);
-            if boundary {
-                // Check for a case where a single key:value pair is equal to or exceeds
-                // the bytes of an individual block. This typically indicates that the average
-                // block size is too small or that the value being stored would be better
-                // represented as a chunked byte array.
-                let one_len_block = block_buf.len() == 1 && block_addrs.is_empty();
-                if one_len_block {
-                    log::warn!(
-                        "writing key & value that exceeds block size, this is highly inefficient"
-                    );
-                }
-                let block_key = block_buf
-                    .first()
-                    .expect("first key impossibly missing")
-                    .0
-                    .clone();
-                let block_addr = self
-                    .leaf_into_node_unchecked(mem::replace(&mut block_buf, Vec::new()))
-                    .await?;
-                block_addrs.push((block_key, block_addr));
-            }
-        }
-        // if there are any remaining key:value pairs, no boundary was found for the
-        // final block - so write them together as the last block in the series.
-        if !block_buf.is_empty() {
-            let block_key = block_buf
-                .first()
-                .expect("first key impossibly missing")
-                .0
-                .clone();
-            let block_addr = self.leaf_into_node_unchecked(block_buf).await?;
-            block_addrs.push((block_key, block_addr));
-        }
-        if block_addrs.len() == 1 {
-            Ok(block_addrs.pop().expect("key:addr impossibly missing").1)
-        } else {
-            self.branches_from_vec(block_addrs).await
-        }
-    }
-    async fn node(mut self, mut kvs: Kvs) -> Result<Addr, Error> {
-        let mut block_addrs = Vec::new();
-        let mut block_buf = Vec::new();
-        for kv in kvs.into_iter() {
-            let boundary = self.roller.roll_bytes(&crate::value::serialize(&kv)?);
-            block_buf.push(kv);
-            if boundary {
-                // Check for a case where a single key:value pair is equal to or exceeds
-                // the bytes of an individual block. This typically indicates that the average
-                // block size is too small or that the value being stored would be better
-                // represented as a chunked byte array.
-                let one_len_block = block_buf.len() == 1 && block_addrs.is_empty();
-                if one_len_block {
-                    log::warn!(
-                        "writing key & value that exceeds block size, this is highly inefficient"
-                    );
-                }
-                let block_key = block_buf
-                    .first()
-                    .expect("first key impossibly missing")
-                    .0
-                    .clone();
-                let block_addr = self
-                    .leaf_into_node_unchecked(mem::replace(&mut block_buf, Vec::new()))
-                    .await?;
-                block_addrs.push((block_key, block_addr));
-            }
-        }
-        // if there are any remaining key:value pairs, no boundary was found for the
-        // final block - so write them together as the last block in the series.
-        if !block_buf.is_empty() {
-            let block_key = block_buf
-                .first()
-                .expect("first key impossibly missing")
-                .0
-                .clone();
-            let block_addr = self.leaf_into_node_unchecked(block_buf).await?;
-            block_addrs.push((block_key, block_addr));
-        }
-        if block_addrs.len() == 1 {
-            Ok(block_addrs.pop().expect("key:addr impossibly missing").1)
-        } else {
-            self.branches_from_vec(block_addrs).await
-        }
-    }
-    async fn leaf_into_node_unchecked(&self, kvs: Vec<(Key, Value)>) -> Result<Addr, Error> {
-        let node = NodeOwned::Leaf(kvs);
-        let (node_addr, node_bytes) = node.as_bytes()?;
-        self.storage.write(node_addr.clone(), &*node_bytes).await?;
-        Ok(node_addr)
+self.recursive_from_kvs(kvs.into()).await
     }
     #[async_recursion::async_recursion]
-    async fn branches_from_vec(&mut self, mut kvs: Vec<(Key, Addr)>) -> Result<Addr, Error> {
-        // Ensure the kvs are sorted - as the trees require sorting.
-        // unstable should be fine, since the keys will (soon) be unique.
-        kvs.sort_unstable_by(|a, b| a.0.cmp(&b.0));
-        // Ensure the kvs are unique.
-        {
-            let maybe_dups_len = kvs.len();
-            kvs.dedup_by(|a, b| a.0 == b.0);
-            if maybe_dups_len != kvs.len() {
-                return Err(Error::Prolly {
-                    message: "cannot construct prolly tree with non-unique keys".to_owned(),
-                });
-            }
-        }
+    async fn recursive_from_kvs(mut self, mut kvs: Kvs) -> Result<Addr, Error> {
         let mut block_addrs = Vec::new();
-        let mut block_buf = Vec::new();
+        let mut block_buf = Kvs::new();
         for kv in kvs.into_iter() {
-            let boundary = self.roller.roll_bytes(&crate::value::serialize(&kv)?);
+            let boundary = self.roller.roll_bytes(&kv.serialize_inner());
             block_buf.push(kv);
             if boundary {
                 // Check for a case where a single key:value pair is equal to or exceeds
@@ -180,14 +72,13 @@ where
                     );
                 }
                 let block_key = block_buf
-                    .first()
-                    .expect("first key impossibly missing")
-                    .0
+                    .first_key()
+                    .expect("buffer impossibly empty")
                     .clone();
-                let block_addr = self
-                    .branch_into_node_unchecked(mem::replace(&mut block_buf, Vec::new()))
-                    .await?;
-                block_addrs.push((block_key, block_addr));
+                let node = mem::replace(&mut block_buf, Kvs::new()).into_node();
+                let (node_addr, node_bytes) = node.as_bytes()?;
+                self.storage.write(node_addr.clone(), &*node_bytes).await?;
+                block_addrs.push((block_key, node_addr));
             }
         }
         // if there are any remaining key:value pairs, no boundary was found for the
@@ -198,20 +89,28 @@ where
                 .expect("first key impossibly missing")
                 .0
                 .clone();
-            let block_addr = self.branch_into_node_unchecked(block_buf).await?;
-            block_addrs.push((block_key, block_addr));
+            let node = mem::replace(&mut block_buf, Kvs::new()).into_node();
+            let (node_addr, node_bytes) = node.as_bytes()?;
+            self.storage.write(node_addr.clone(), &*node_bytes).await?;
+            let block_kv = self.write_block(mem::replace(&mut block_buf, Kvs::new())
+            block_addrs.push((block_key, node_addr));
         }
         if block_addrs.len() == 1 {
             Ok(block_addrs.pop().expect("key:addr impossibly missing").1)
         } else {
-            self.branches_from_vec(block_addrs).await
+            self.recursive_from_kvs(block_addrs.into()).await
         }
     }
-    async fn branch_into_node_unchecked(&self, kvs: Vec<(Key, Addr)>) -> Result<Addr, Error> {
-        let node = NodeOwned::Branch(kvs);
-        let (node_addr, node_bytes) = node.as_bytes()?;
-        self.storage.write(node_addr.clone(), &*node_bytes).await?;
-        Ok(node_addr)
+    async fn write_block(&self, block_buf: Kvs) -> Result<(Key, Addr), Error> {
+            let key = block_buf
+                .first()
+                .expect("first key impossibly missing")
+                .0
+                .clone();
+            let node = block_buf.into_node();
+            let (node_addr, node_bytes) = node.as_bytes()?;
+            self.storage.write(node_addr.clone(), &*node_bytes).await?;
+            Ok((key, node_addr))
     }
 }
 #[cfg(test)]
