55e136682e5f227cc22715ea3a254e4199043440
Lee Olayvar <leeola@fastmail.com>
2020-11-07 15:40:35 -0800

    trying out nom for parsing user inputs

--- src/value/from_cli_str.rs
+++ src/value/from_cli_str.rs
@@ -1,4 +1,14 @@
-use super::{Key, Path, Scalar, Value};
+use {
+    super::{Addr, Key, Path, Scalar, Value},
+    nom::{
+        branch::alt,
+        bytes::complete::tag_no_case,
+        character::complete::{alphanumeric1, digit1},
+        combinator::{map, map_res},
+        sequence::preceded,
+        IResult,
+    },
+};
 
 impl Scalar {
     /// An experimental implementation to parse a [`Scalar`] value from a string
@@ -6,7 +16,7 @@ impl Scalar {
     ///
     /// This differs from a `FromStr` implementation in that there may be multiple
     /// interfaces tailored towards different user interfaces.
-    pub fn from_cli_str(s: &str) -> Result<Self, Error> {
+    pub fn from_cli_str(__s: &str) -> Result<Self, Error> {
         todo!("Scalar from cli str")
     }
 }
@@ -16,7 +26,7 @@ impl Value {
     ///
     /// This differs from a `FromStr` implementation in that there may be multiple
     /// interfaces tailored towards different user interfaces.
-    pub fn from_cli_str(s: &str) -> Result<Self, Error> {
+    pub fn from_cli_str(__s: &str) -> Result<Self, Error> {
         todo!("Value from cli str")
     }
 }
@@ -26,7 +36,7 @@ impl Key {
     ///
     /// This differs from a `FromStr` implementation in that there may be multiple
     /// interfaces tailored towards different user interfaces.
-    pub fn from_cli_str(s: &str) -> Result<Self, Error> {
+    pub fn from_cli_str(__s: &str) -> Result<Self, Error> {
         todo!("Key from cli str")
     }
 }
@@ -36,7 +46,7 @@ impl Path {
     ///
     /// This differs from a `FromStr` implementation in that there may be multiple
     /// interfaces tailored towards different user interfaces.
-    pub fn from_cli_str(s: &str) -> Result<Self, Error> {
+    pub fn from_cli_str(__s: &str) -> Result<Self, Error> {
         todo!("Path from cli str")
     }
 }
@@ -45,4 +55,65 @@ impl Path {
 pub enum Error {
     #[error("invalid u32: `{0}`")]
     InvalidUint32(String),
+}
+
+fn parse_uint32(input: &str) -> IResult<&str, u32> {
+    map_res(digit1, |s| u32::from_str_radix(s, 10))(input)
+}
+fn parse_string(input: &str) -> IResult<&str, String> {
+    Ok(("", input.to_owned()))
+}
+fn parse_addr(input: &str) -> IResult<&str, Addr> {
+    // all addrs are alphanum currently, so we may as well
+    // enforce it.
+    map(alphanumeric1, Addr::from)(input)
+}
+fn parse_untyped_scalar(input: &str) -> IResult<&str, Scalar> {
+    alt((
+        map(parse_uint32, Scalar::Uint32),
+        map(parse_string, Scalar::String),
+    ))(input)
+}
+fn parse_typed_scalar(input: &str) -> IResult<&str, Scalar> {
+    // TODO: check if it has a foo: prefix? Otherwise these are being checked
+    // needlessly.
+    alt((
+        preceded(tag_no_case("u32:"), map(parse_uint32, Scalar::Uint32)),
+        preceded(tag_no_case("str:"), map(parse_string, Scalar::String)),
+        preceded(tag_no_case("addr:"), map(parse_addr, Scalar::Addr)),
+    ))(input)
+}
+fn parse_scalar(input: &str) -> IResult<&str, Scalar> {
+    alt((parse_typed_scalar, parse_untyped_scalar))(input)
+}
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    #[test]
+    fn untyped_scalar() {
+        assert_eq!(
+            parse_untyped_scalar("foo"),
+            Ok(("", Scalar::String("foo".into()))),
+        );
+        assert_eq!(parse_untyped_scalar("5"), Ok(("", Scalar::Uint32(5))));
+        assert_eq!(parse_untyped_scalar("505"), Ok(("", Scalar::Uint32(505))));
+    }
+    #[test]
+    fn typed_scalar() {
+        assert_eq!(
+            parse_typed_scalar("str:foo"),
+            Ok(("", Scalar::String("foo".into()))),
+        );
+        assert_eq!(parse_typed_scalar("u32:5"), Ok(("", Scalar::Uint32(5))));
+    }
+    #[test]
+    fn scalar() {
+        assert_eq!(parse_scalar("foo"), Ok(("", Scalar::String("foo".into()))),);
+        assert_eq!(parse_scalar("5"), Ok(("", Scalar::Uint32(5))));
+        assert_eq!(
+            parse_scalar("str:foo"),
+            Ok(("", Scalar::String("foo".into()))),
+        );
+        assert_eq!(parse_scalar("u32:5"), Ok(("", Scalar::Uint32(5))));
+    }
 }
