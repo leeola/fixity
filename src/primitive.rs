3de34ab94f94f2e6340a7a9e025de7be676ba0b1
Lee Olayvar <leeola@fastmail.com>
2021-02-17 16:34:02 -0800

    refactor: remove old Chain impl
    `Chain` is an exploration into type-based commit behavior. Where in
    an inner value is wrapped, with a hierarchy of types which commit
    a tree of changes.
    
    This model was difficult and assumed control of the commit process
    from interfaces (Map, Bytes, etc).
    
    Functionality of tree-updates have been primarily moved to
    `crate::Path` and `crate::path::Segment`.

--- src/primitive.rs
+++ src/primitive.rs
@@ -2,35 +2,11 @@
 
 pub mod appendlog;
 pub mod bytes;
-pub mod chain;
 pub mod commitlog;
 pub mod prollylist;
 pub mod prollytree;
-use crate::{
-    value::{Addr, Key},
-    Error,
-};
 pub use {
     appendlog::AppendLog,
     bytes::{Create as BytesCreate, Read as BytesRead},
-    commitlog::{Commit, CommitLog},
+    commitlog::CommitLog,
 };
-
-// TODO: kill
-#[async_trait::async_trait]
-pub trait Flush {
-    async fn flush(&mut self) -> Result<Addr, Error>;
-}
-#[async_trait::async_trait]
-pub trait Build {
-    type Primitive;
-    async fn build(self, addr: Option<Addr>) -> Result<Self::Primitive, Error>;
-}
-#[async_trait::async_trait]
-pub trait InsertAddr {
-    async fn insert_addr(&mut self, key: Key, addr: Addr) -> Result<(), Error>;
-}
-#[async_trait::async_trait]
-pub trait GetAddr {
-    async fn get_addr(&self, key: Key) -> Result<Option<Addr>, Error>;
-}
--- src/primitive/commitlog.rs
+++ src/primitive/commitlog.rs
@@ -1,6 +1,6 @@
 use {
     crate::{
-        primitive::{appendlog::LogContainer, AppendLog, Flush},
+        primitive::{appendlog::LogContainer, AppendLog},
         storage::{StorageRead, StorageWrite},
         Addr, Error,
     },
@@ -23,9 +23,6 @@ impl<'s, S> CommitLog<'s, S> {
     pub fn new(storage: &'s S, addr: Option<Addr>) -> Self {
         let log = AppendLog::new(storage, addr);
         Self { log }
-    }
-    pub fn wrap_inner<Inner>(self, inner: Inner) -> Commit<'s, S, Inner> {
-        Commit { log: self, inner }
     }
 }
 impl<'s, S> CommitLog<'s, S>
@@ -69,31 +66,5 @@ where
                 content,
             })
             .await
-    }
-}
-pub struct Commit<'s, S, Inner> {
-    log: CommitLog<'s, S>,
-    inner: Inner,
-}
-#[async_trait::async_trait]
-impl<'s, S, Inner> Flush for Commit<'s, S, Inner>
-where
-    S: StorageRead + StorageWrite,
-    Inner: Flush + Sync + Send,
-{
-    async fn flush(&mut self) -> Result<Addr, Error> {
-        let content = self.inner.flush().await?;
-        self.log.append(content).await
-    }
-}
-impl<'s, S, Inner> std::ops::Deref for Commit<'s, S, Inner> {
-    type Target = Inner;
-    fn deref(&self) -> &Self::Target {
-        &self.inner
-    }
-}
-impl<'s, S, Inner> std::ops::DerefMut for Commit<'s, S, Inner> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.inner
     }
 }
