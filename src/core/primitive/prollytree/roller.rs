b6eba7a1d930586b6f6559818f9bc27ec94dfc24
Lee Olayvar <leeola@fastmail.com>
2021-04-19 16:38:58 -0700

    docs: roller, boyscout rule
    I was a bit confused by the roller impl, so i left the campsite
    cleaner than i found it.

--- src/core/primitive/prollytree/roller.rs
+++ src/core/primitive/prollytree/roller.rs
@@ -9,6 +9,11 @@ pub struct Config {
     pub window_size: u32,
 }
 impl Config {
+    /// The pattern to look for in the [`Roller`].
+    ///
+    /// Typically a series of all `1` bits, with the width indicating the probability.
+    ///
+    /// See also: [`Roller::roll_byte`].
     pub fn with_pattern(pattern: u32) -> Self {
         Self {
             pattern,
@@ -43,9 +48,41 @@ impl Roller {
             buzhash: BuzHash::new(window_size),
         }
     }
+    /// Roll the given byte and return whether or not it matches the underlying pattern.
+    ///
+    /// # High Level Explanation
+    ///
+    /// Two primary principles are at play with the `Roller`.
+    ///
+    /// 1. Hashing of input data, a single byte in this case.
+    /// 2. Checking if the new rolling hash result matches a pattern.
+    ///
+    /// Hashing is basic, we expect a evenly distributed hash based on the input data.
+    /// Super standard.
+    ///
+    /// The pattern matching however is written in such a way that the "pattern" is used to
+    /// effectively truncate the resulting hash. The truncated value is then equality compared to
+    /// the pattern, and if matching, the input byte matched the pattern.
+    ///
+    /// This truncate and match allows the pattern to be of a variable size and complexity. This is
+    /// useful to easily change the likihood of a pattern match. The smaller the pattern the
+    /// more likely a pattern match is found.
+    ///
+    /// # Pattern Matching
+    ///
+    /// Pattern matching is a basic Bitwise `AND` to truncate the hash by the pattern, which are
+    /// expected to be a high bit set of `1`s. Eg a pattern of `0b111` would truncate `0b10111`
+    /// to `0b101`. This behavior means that a pattern match of `0b111` has a 1 in 8 chance of
+    /// occuring, with a probability of `1/2^8`. The probability is thereby configurable based
+    /// on the chosen bit width, aka 8 in that example. A larger bit width would be less likely
+    /// to occur, resulting in wider chunk sizes.
     pub fn roll_byte(&mut self, b: u8) -> bool {
         self.buzhash.hash_byte(b) & self.pattern == self.pattern
     }
+    /// Roll the given bytes and return whether or not it matches the underlying pattern.
+    ///
+    /// For conceptual documentation, see the single-byte version of this method,
+    /// [`Roller::roll_byte`].
     pub fn roll_bytes(&mut self, bytes: &[u8]) -> bool {
         for &b in bytes {
             if self.roll_byte(b) {
