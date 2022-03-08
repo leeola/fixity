6f8297c84f41e389a5d6ff4ed75bb59843bfedac
Lee Olayvar <leeola@fastmail.com>
2022-03-07 21:55:07 -0800

    wip, stash value schema toying

--- fixity_store/src/lib.rs
+++ fixity_store/src/lib.rs
@@ -1,3 +1,24 @@
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Scalar {
+    Addr,
+    Uint32(u32),
+    String(String),
+}
+pub enum Type {
+    GCounter,
+    CascadingCounter(Box<Type>),
+}
+pub enum Struct {
+    GCounter(GCounter),
+    LACounter(LACounter),
+}
+pub struct GCounter;
+pub struct LACounter {
+    limit: usize,
+    counter: GCounter,
+    // Just storing this temporarily in a Result for ease
+    alternate: Result<GCounter, Box<LACounter>>,
+}
 #[cfg(test)]
 mod tests {
     #[test]
