cb651ac2a7504a559d97f8d908b0e74666b9dc6f
Lee Olayvar <leeola@fastmail.com>
2022-04-09 15:12:22 -0700

    wip, stash 1...

--- fixity_store/src/store.rs
+++ fixity_store/src/store.rs
@@ -16,7 +16,7 @@ pub type Error = ();
 //     async fn get<T>(&self) -> Self::Ref<T>;
 // }
 
-const ADDR_LENGTH: usize = 32;
+const ADDR_LENGTH: usize = 34;
 pub type Addr = [u8; ADDR_LENGTH];
 
 #[async_trait::async_trait]
@@ -33,11 +33,11 @@ pub trait Store<T, A = Addr> {
 pub trait StoreRef<T> {
     type Repr;
     fn repr_to_owned(&self) -> Result<T, Error>;
-    fn repr_borrow<U: ?Sized>(&self) -> Result<&U, Error>
+    fn repr_borrow<'a, U: ?Sized>(&'a self) -> Result<&'a U, Error>
     where
         Self::Repr: Borrow<U>;
 }
-pub mod test_store {
+pub mod any_store {
     use {
         super::{Addr, Error, Store, StoreRef},
         std::{
@@ -54,21 +54,21 @@ pub mod test_store {
         _phantom: PhantomData<T>,
     }
     type DynRef = Arc<dyn Any + Send + Sync>;
-    pub struct TestStore<A = Addr>(Mutex<HashMap<A, DynRef>>);
-    impl TestStore {
+    pub struct AnyStore<A = Addr>(Mutex<HashMap<A, DynRef>>);
+    impl AnyStore {
         pub fn new() -> Self {
             Self(Mutex::new(HashMap::new()))
         }
     }
     #[async_trait::async_trait]
-    impl<T, A> Store<T, A> for TestStore<A>
+    impl<T, A> Store<T, A> for AnyStore<A>
     where
         T: Any + Clone + Send + Sync + 'static,
         A: From<Addr> + Clone + Hash + Eq + Send + Sync,
     {
         type Ref = AnyRef<T>;
         async fn put(&self, t: T) -> Result<A, ()> {
-            let key = A::from([0u8; 32]);
+            let key = A::from([0u8; 34]);
             self.0.lock().unwrap().insert(key.clone(), Arc::new(t));
             Ok(key)
         }
@@ -103,11 +103,13 @@ pub mod test_store {
         }
     }
 }
+/*
 pub mod json_store {
     use {
         super::{Addr, Error, Store, StoreRef},
+        multihash::MultihashDigest,
+        serde::{de::DeserializeOwned, Deserialize, Serialize},
         std::{
-            any::Any,
             borrow::Borrow,
             collections::HashMap,
             hash::Hash,
@@ -129,17 +131,21 @@ pub mod json_store {
     #[async_trait::async_trait]
     impl<T, A> Store<T, A> for JsonStore<A>
     where
-        T: Any + Clone + Send + Sync + 'static,
-        A: From<Addr> + Clone + Hash + Eq + Send + Sync,
+        T: Serialize + DeserializeOwned + Send + Sync + 'static,
+        A: TryFrom<Vec<u8>> + Clone + Hash + Eq + Send + Sync,
     {
         type Ref = JsonRef<T>;
-        async fn put(&self, t: T) -> Result<A, ()> {
-            let addr = A::from([0u8; 32]);
-            let buf: Vec<u8> = todo!("serialize T");
+        async fn put(&self, t: T) -> Result<A, Error> {
+            let buf: Vec<u8> = serde_json::to_vec(&t).unwrap();
+            let addr: A = multihash::Code::Blake3_256
+                .digest(&buf)
+                .to_bytes()
+                .try_into()
+                .map_err(|_| ())?;
             self.0.lock().unwrap().insert(addr.clone(), Arc::from(buf));
             Ok(addr)
         }
-        async fn get(&self, cid: &A) -> Result<Self::Ref, ()> {
+        async fn get(&self, cid: &A) -> Result<Self::Ref, Error> {
             let buf = {
                 let map = self.0.lock().unwrap();
                 Arc::clone(&map.get(cid).unwrap())
@@ -152,28 +158,32 @@ pub mod json_store {
     }
     impl<T> StoreRef<T> for JsonRef<T>
     where
-        T: Any + Clone,
+        T: DeserializeOwned,
     {
         type Repr = T;
         fn repr_to_owned(&self) -> Result<T, Error> {
-            todo!()
+            use std::ops::Deref;
+            let t: T = serde_json::from_slice(self.buf.deref()).unwrap();
+            Ok(t)
         }
-        fn repr_borrow<U: ?Sized>(&self) -> Result<&U, Error>
+        fn repr_borrow<'a, U: ?Sized>(&'a self) -> Result<&'a U, Error>
         where
             Self::Repr: Borrow<U>,
+            U: Deserialize<'a>,
         {
             todo!()
         }
     }
 }
+*/
 #[cfg(test)]
 pub mod test {
     use {
-        super::{json_store::JsonStore, test_store::TestStore, *},
+        super::{any_store::AnyStore, json_store::JsonStore, *},
         rstest::*,
     };
     #[rstest]
-    #[case::test_any_store(TestStore::new())]
+    #[case::test_any_store(AnyStore::new())]
     #[case::test_any_store(JsonStore::new())]
     #[tokio::test]
     async fn store_poc<S>(#[case] store: S)
