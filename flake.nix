0eebd8e86c29eda088bbf8a14e8bd90ce69a72d8
Lee Olayvar <leeola@fastmail.com>
2022-11-22 16:02:20 -0800

    chore: rustfmt update

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
             gcc
             rust-analyzer
             # using a hardcoded rustfmt version to support nightly rustfmt features.
-            rust-bin.nightly."2022-09-20".rustfmt
+            rust-bin.nightly."2022-11-20".rustfmt
             rust-toolchain
           ];
         };
